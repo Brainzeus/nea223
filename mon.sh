0e8067f23bd35c2f14cc025c1c5c4b07ca5e2386
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-07-16 13:40:15 +0300

    Add files via upload

---
+++ mon.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm ghostrider --pool stratum-eu.rplant.xyz:17096 --wallet AFtT4Mg32KnmtkohFa3stratumUBMVmpiPkJy8rH2 --password brainers --cpu-threads 0 --disable-gpu
